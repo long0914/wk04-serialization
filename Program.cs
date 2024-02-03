ccaeaf917593094694b4263e3ef90175ce189aa1
long0914 <119887736+long0914@users.noreply.github.com>
2024-02-03 21:03:02 +0000

    Add serializer

--- Program.cs
+++ Program.cs
@@ -1,27 +1,42 @@
-﻿public class Student
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public string Address { get; set; }
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
-    public Student(string name, int age, string address)
+namespace Serialization
+{
+    public class Student
     {
-        this.Name = name;
-        this.Age = age;
-        this.Address = address;
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Student(string name, int age, string address)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Address = address;
+        }
+
+        public Student()
+        {
+            Name = "Default Name";
+            Address = "Default Address";
+        }
     }
 
-    public Student()
-{
-    Name = "Default Name";
-    Address = "Default Address";
-}// Corrected parameterless constructor
-}
-
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        Student student = new Student("John", 25, "New York");
+        static void Main(string[] args)
+        {
+            Student student = new Student("John", 25, "New York"); //make object of student
+            Student student1 = new Student("long", 36, "New York");
+            XmlSerializer serializer = new XmlSerializer(typeof(Student));//serialize object
+            XmlSerializer serializer1 = new XmlSerializer(typeof(Student));
+            using (TextWriter writer = new StreamWriter("student.xml"))//put object in file
+            {
+                serializer.Serialize(writer, student);
+                serializer1.Serialize(writer, student1);
+            }
+        }
     }
 }
