d33cff630b8437168288d7de9faafde6baa3f1a1
long0914 <119887736+long0914@users.noreply.github.com>
2024-02-03 20:55:07 +0000

    add xml

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+
+    public Student(string name, int age, string address)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Address = address;
+    }
+
+    public Student()
+{
+    Name = "Default Name";
+    Address = "Default Address";
+}// Corrected parameterless constructor
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student("John", 25, "New York");
+    }
+}
